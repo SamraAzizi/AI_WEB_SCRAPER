278c018f682f4e9c72692400a2db7f690ad0cd28
Samra <fahimaazizi369@gmail.com>
2025-04-11 15:27:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import streamlit as st
 
 
@@ -8,8 +8,3 @@ url = st.text_input("Enter a website URL: ")
 if st.button("Scrape Site"):
     st.write("Scraping the website")
 
-
-    
-=======
-
->>>>>>> 492ab7610bb08123bb5f3c1ec1b60a839db4630e
