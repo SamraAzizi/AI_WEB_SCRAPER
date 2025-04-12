d36cb3d4089a81b2ad599c01070841f3fac75e53
Samra <fahimaazizi369@gmail.com>
2025-04-12 16:39:22 +0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -61,15 +61,3 @@ def split_dom_content(dom_content, max_length=6000):
     ]
 
 
-import selenium.webdrver as webdriver
-from selenium.webdriver.chrome.service import Service
-import time
-
-def scrape_website(website):
-    print("Launchig chrome browser...")
-
-    chrome_driver_path = "./chromedriver.exe"
-    options = webdriver.ChromeOption()
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
-
- 
