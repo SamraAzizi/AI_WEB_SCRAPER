08933603c6166eedeee9e8eb09a5ed75ad413c60
Samra <fahimaazizi369@gmail.com>
2025-04-13 15:02:05 +0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,9 +1,13 @@
-
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
+template = (
+    "You are tasked with extraction specific information from the following text context {dom_context}."
+    "Please follow these instruction carefully : \n\n"
+    "1. **Extract Information:** Only extract "
+
+)
 
 model = OllamaLLM(model="llama2")
 
 def parse_with_ollama(dom_chunks, parse_description):
-    
