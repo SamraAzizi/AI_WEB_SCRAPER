17b146cd7c5f2b035ef985fe0e3d55fa57478ab2
Samra <fahimaazizi369@gmail.com>
2025-04-13 11:42:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
 import streamlit as st
-from scrape import scrape_website
+from scrape import (
+    scrape_website,
+    split_dom_content,
+    clean_body_content,
+    extract_body_content,
+)
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
@@ -8,5 +13,7 @@ url = st.text_input("Enter a website URL: ")
 if st.button("Scrape Site"):
     st.write("Scraping the website")
     result = scrape_website(url)
+    body_content = extract_body_content(result)
+    clean_content = clean_body_content(body_content)
     print(result)
 
