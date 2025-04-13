f60e96567f86f66cd6b00ba03e89b72054e5f719
Samra <fahimaazizi369@gmail.com>
2025-04-13 13:45:29 +0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,1 +1,9 @@
 
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+
+model = OllamaLLM(model="llama2")
+
+def parse_with_ollama(dom_chunks, parse_description):
+    
