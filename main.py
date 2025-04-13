f9f8b106617314b807f368337a383122146068f1
Samra <fahimaazizi369@gmail.com>
2025-04-13 12:08:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,19 @@ if st.button("Scrape Site"):
     result = scrape_website(url)
     body_content = extract_body_content(result)
     clean_content = clean_body_content(body_content)
-    print(result)
+    
 
+    st.session_state.dom_content = clean_content
+
+    with st.expander("View Dom Content"):
+        st.text_area("DOM CONTENT", clean_content, height=300)
+
+
+
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            
