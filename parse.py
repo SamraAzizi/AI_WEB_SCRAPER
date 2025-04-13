310cfeaa37066c6808b66241de05fd5d5f577212
Samra <fahimaazizi369@gmail.com>
2025-04-13 15:04:33 +0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -4,7 +4,10 @@ from langchain_core.prompts import ChatPromptTemplate
 template = (
     "You are tasked with extraction specific information from the following text context {dom_context}."
     "Please follow these instruction carefully : \n\n"
-    "1. **Extract Information:** Only extract "
+    "1. **Extract Information:** Only extract the information directly matches the provided description: {parse_description}"
+    "2. **No Extra Content:** Do not include any additional text, comments, or explanations in your response. "
+    "3. **Empty Response:** If no information matches the description, return an empty string ('')."
+    "4. **Direct Data Only:** Your output should contain only the data that is explicitly requested, with no other text.""
 
 )
 
